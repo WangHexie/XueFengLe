4fca30e0cbc8f61773ee4fb68a4a54031ec32de1
WangHexie <31768052+WangHexie@users.noreply.github.com>
2021-05-10 22:24:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ cookies = sys.argv[1]
 header = {
     "Accept": "application/json, text/plain, */*",
     "Origin": "http://web.lngqt.shechem.cn",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1316.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1320.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63010200)",
     "Content-Type": "application/x-www-form-urlencoded",
     "Referer": "http://web.lngqt.shechem.cn/study",
     "Accept-Encoding": "gzip, deflate",
